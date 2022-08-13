636d5c6aa160173fdb55e8e332e2968a5e481fa0
Colin Waddell <mrcolin@gmail.com>
2022-08-13 17:22:30 +0100

    first pass on a LED blinker

---
+++ scenes/loadingled.py
@@ -0,0 +1,48 @@
+from utilities.animator import Animator
+import RPi.GPIO as GPIO
+
+# Attempt to load config data
+try:
+    from config import LOADING_LED_GPIO_PIN
+
+except (ModuleNotFoundError, NameError, ImportError):
+    # If there's no config data
+    LOADING_LED_GPIO_PIN = 25
+
+BLINKER_STEPS = 10
+
+class LoadingLEDScene(object):
+    def __init__(self):
+        # Setup GPIO for blinking
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(LOADING_LED_GPIO_PIN, GPIO.OUT)
+
+        # Setup PWM
+        self._pwm  = GPIO.PWM(LOADING_LED_GPIO_PIN, 50)
+        self._pwm.start(100)
+
+        super().__init__()
+
+    def __del__(self):
+        GPIO.cleanup()
+        super().__del__()
+
+    @Animator.KeyFrame.add(2)
+    def loading_led(self, count):
+        reset_count = True
+        if self.overhead.processing:
+            # Calculate the brightness scaler and
+            # ensure it's within a sensible range
+            brightness = (1 - (count / BLINKER_STEPS)) / 2
+            brightness = 0 if (brightness < 0 or brightness > 1) else brightness
+
+            # Set LED to brightness
+            self._pwm.start(100 * brightness)
+
+            # Only count 0 -> (BLINKER_STEPS - 1)
+            reset_count = count == (BLINKER_STEPS - 1)
+        else:
+            # Not processing, leave LED on
+            self._pwm.start(100)
+
+        return reset_count
