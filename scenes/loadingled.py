66276546289942e0fbc56cecade33cc63374f9db
Colin Waddell <mrcolin@gmail.com>
2024-01-04 09:56:14 +0000

    only use GPIO if setup has been successful, else retry setup. I suspect this will still not work and it's the library import where the problem starts

--- scenes/loadingled.py
+++ scenes/loadingled.py
@@ -14,23 +14,36 @@ BLINKER_STEPS = 4
 
 class LoadingLEDScene(object):
     def __init__(self):
-        # Setup GPIO for blinking
-        GPIO.setwarnings(False)
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(LOADING_LED_GPIO_PIN, GPIO.OUT)
-        GPIO.output(LOADING_LED_GPIO_PIN, GPIO.HIGH)
+        self.gpio_setup_complete = False
+        self.gpio_setup()
 
         super().__init__()
+    
+    def gpio_setup(self):
+        if not self.gpio_setup_complete:
+            try:
+                # Setup GPIO for blinking
+                GPIO.setwarnings(False)
+                GPIO.setmode(GPIO.BCM)
+                GPIO.setup(LOADING_LED_GPIO_PIN, GPIO.OUT)
+                GPIO.output(LOADING_LED_GPIO_PIN, GPIO.HIGH)
+                self.gpio_setup_complete = True
+
+            except:
+                self.gpio_setup_complete = False
 
     @Animator.KeyFrame.add(4)
     def loading_led(self, count):
-        reset_count = True
-        if self.overhead.processing:
-            GPIO.output(
-                LOADING_LED_GPIO_PIN,
-                GPIO.HIGH if count % 2 else GPIO.LOW
-            )
+        if self.gpio_setup_complete:
+            reset_count = True
+            if self.overhead.processing:
+                GPIO.output(
+                    LOADING_LED_GPIO_PIN,
+                    GPIO.HIGH if count % 2 else GPIO.LOW
+                )
 
+            else:
+                # Not processing, leave LED on
+                GPIO.output(LOADING_LED_GPIO_PIN, GPIO.HIGH)
         else:
-            # Not processing, leave LED on
-            GPIO.output(LOADING_LED_GPIO_PIN, GPIO.HIGH)
+            self.gpio_setup()
