8e4131a839b254d8cae726e996d7a129235debc1
Colin Waddell <mrcolin@gmail.com>
2024-01-04 11:00:55 +0000

    a little less aggresive

--- scenes/loadingled.py
+++ scenes/loadingled.py
@@ -1,5 +1,7 @@
 from utilities.animator import Animator
 from setup import frames
+from time import sleep
+import RPi.GPIO as GPIO
 
 # Attempt to load config data
 try:
@@ -10,42 +12,38 @@ except (ModuleNotFoundError, NameError, ImportError):
     LOADING_LED_GPIO_PIN = 25
 
 BLINKER_STEPS = 4
-GPIO = None
 
 class LoadingLEDScene(object):
     def __init__(self):
         self.gpio_setup_complete = False
         self.gpio_setup()
-
         super().__init__()
-    
-    def gpio_setup(self):
-        if not self.gpio_setup_complete:
-            try:
-                import RPi.GPIO
-                GPIO = RPi.GPIO
-                # Setup GPIO for blinking
-                GPIO.setwarnings(False)
-                GPIO.setmode(GPIO.BCM)
-                GPIO.setup(LOADING_LED_GPIO_PIN, GPIO.OUT)
-                GPIO.output(LOADING_LED_GPIO_PIN, GPIO.HIGH)
-                self.gpio_setup_complete = True
 
-            except:
-                self.gpio_setup_complete = False
+    def gpio_setup(self):
+        try:
+            GPIO.setwarnings(False)
+            GPIO.setmode(GPIO.BCM)
+            GPIO.setup(LOADING_LED_GPIO_PIN, GPIO.OUT)
+            GPIO.output(LOADING_LED_GPIO_PIN, GPIO.HIGH)
+            self.gpio_setup_complete = True
+        except:
+            self.gpio_setup_complete = False
 
     @Animator.KeyFrame.add(4)
     def loading_led(self, count):
-        if self.gpio_setup_complete and not GPIO is None:
-            reset_count = True
-            if self.overhead.processing:
+        reset_count = True
+
+        if not self.gpio_setup_complete:
+            self.gpio_setup()
+
+        if self.overhead.processing:
+            if self.gpio_setup_complete:
                 GPIO.output(
                     LOADING_LED_GPIO_PIN,
                     GPIO.HIGH if count % 2 else GPIO.LOW
                 )
 
-            else:
-                # Not processing, leave LED on
-                GPIO.output(LOADING_LED_GPIO_PIN, GPIO.HIGH)
         else:
-            self.gpio_setup()
+            # Not processing, leave LED on
+            if self.gpio_setup_complete:
+                GPIO.output(LOADING_LED_GPIO_PIN, GPIO.HIGH)
