e87f058cce79a8b6ca75b51c7b112874d9623329
User Name <first@example.com>
2024-04-27 11:04:33 -0400

    merge legacy colors

--- scenes/clock.py
+++ scenes/clock.py
@@ -8,7 +8,7 @@ from rgbmatrix import graphics
 # Setup
 CLOCK_FONT = fonts.regular
 CLOCK_POSITION = (1, 8)
-CLOCK_COLOUR = colours.BLUE_DARK
+CLOCK_COLOUR = colours.GOLDENROD
 
 
 class ClockScene(object):
